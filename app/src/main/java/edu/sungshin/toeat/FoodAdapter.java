cd20273e529b4b809c9fbcbff1fd216e9a079233
허예은 <20211415@sungshin.ac.kr>
2022-11-04 14:37:40 +0900

    DB insert finish, delete ing

--- app/src/main/java/edu/sungshin/toeat/FoodAdapter.java
+++ app/src/main/java/edu/sungshin/toeat/FoodAdapter.java
@@ -1,20 +1,28 @@
 package edu.sungshin.toeat;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
     ArrayList<Food> items=new ArrayList<>();
+
+    public FoodAdapter(ArrayList<Food> foodList){ items=foodList;}//생성자:전달한 FoodList 받아 items리스트에 대입
 
     public void addItem(Food item){
         items.add(item);
@@ -49,6 +57,7 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
 
     static class ViewHolder extends RecyclerView.ViewHolder{
         ImageView imageView;
+        Button foodDelete;
         TextView textView,textView2,textView3,textView4,textView5;
 
         View diaLogView;
@@ -58,6 +67,7 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
             super(itemView);
 
             imageView=itemView.findViewById(R.id.imageView);
+            foodDelete=itemView.findViewById(R.id.deleteBtn);
             textView=itemView.findViewById(R.id.textView);
             textView2=itemView.findViewById(R.id.textView2);
             textView3=itemView.findViewById(R.id.textView3);
@@ -87,6 +97,17 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
                     dlg.setPositiveButton("확인",null);
                     dlg.setNegativeButton("취소",null);
                     dlg.show();
+                }
+            });
+
+            foodDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    UserAdapter mDbHelper=new UserAdapter(foodDelete.getContext());
+                    mDbHelper.createDatabase();
+                    mDbHelper.open();
+                    mDbHelper.delete(textView.getText().toString());
+                    //notifyDataSetChanged
                 }
             });
         }
